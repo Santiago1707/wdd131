eee6031ba8897248327983e2c0546db1fd15fe85
Santiago1707 <dmendozaladino@byupathway.edu>
2025-01-17 22:01:52 -0500

    java add

--- temples.js
+++ temples.js
@@ -5,3 +5,11 @@ document.getElementById('currentyear').textContent = currentYear;
 // Get the last modified date
 const lastModified = document.lastModified;
 document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;
+
+// Hamburger menu functionality
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+});
