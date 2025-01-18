473e7dba4575d8281912ef8b4dc69f6126a36a95
Santiago1707 <dmendozaladino@byupathway.edu>
2025-01-18 16:28:04 -0500

    almost finish

--- temples.js
+++ temples.js
@@ -11,7 +11,6 @@ const mainnav = document.querySelector('.navigation');
 const hambutton = document.querySelector('#menu');
 
 hambutton.addEventListener('click', () => {
-    mainnav.classList.toggle('show');
-    hambutton.classList.toggle('open');
-    hambutton.textContent = hambutton.classList.contains('open') ? 'X' : '☰';
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
 });
